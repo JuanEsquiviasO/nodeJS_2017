804c4f531e5d2bba5df0ab84b93fc92ce81532ca
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-05-26 14:15:52 -0500

    models in Node.js vs others

---
+++ 00-AsyncModel/1-blocking.php
@@ -0,0 +1,14 @@
+
+<?php
+print('Abriendo Archivo...<br>');
+$fichero_url = fopen('archivo.txt', 'r');
+
+$texto = '';
+
+while ( $trozo = fgets($fichero_url) ) {
+    $texto .= $trozo;
+}
+
+print($texto);
+
+print('<br>Haciendo otra cosa');
